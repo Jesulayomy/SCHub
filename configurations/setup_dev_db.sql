c36dd478d264e24f2c1d5b4f3f665005a61dd27a
Jesulayomy <jesulayomi081@gmail.com>
2025-01-27 09:40:40 -0500

    [Configs]: Test and dev SQL db setup scripts adjusted to check if db exists

--- configurations/setup_dev_db.sql
+++ configurations/setup_dev_db.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS schub;
 CREATE DATABASE IF NOT EXISTS schub;
 CREATE USER IF NOT EXISTS "schub_dev"@"localhost" IDENTIFIED BY "schub_dev_pwd";
 GRANT ALL PRIVILEGES ON schub.* TO "schub_dev"@"localhost";
--- configurations/setup_test_db.sql
+++ configurations/setup_test_db.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS schub_test_db;
 CREATE DATABASE IF NOT EXISTS schub_test_db;
 CREATE USER IF NOT EXISTS "schub_test"@"localhost" IDENTIFIED BY "schub_test_pwd";
 GRANT ALL PRIVILEGES ON schub_test_db.* TO "schub_test"@"localhost";
