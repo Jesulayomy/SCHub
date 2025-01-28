a8e94fabe10facfb49002d64dd455a5772cac5e7
Jesulayomy <jesulayomi081@gmail.com>
2025-01-28 12:59:03 -0500

    [Data]: Removed unnecesssary files

--- data/setup_test_db.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE schub_test_db;
-CREATE DATABASE IF NOT EXISTS schub_test_db;
-CREATE USER IF NOT EXISTS "schub_test"@"localhost" IDENTIFIED BY "SECURE1_TEST2_PASSWORD3";
-GRANT ALL PRIVILEGES ON schub_test_db.* TO "schub_test"@"localhost";
