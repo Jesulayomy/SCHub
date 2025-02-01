1c50335f6a300f620b933c686fd019bc19259f7c
Jesulayomy <jesulayomi081@gmail.com>
2025-02-01 15:39:33 -0500

    [Settings]: Token blacklist app, migrations

--- app/schub/settings.py
+++ app/schub/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "drf_spectacular",
     "core.apps.CoreConfig",
